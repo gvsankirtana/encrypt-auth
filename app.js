9594ab047e6917033cce01bf9f1675ce6793a234
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-06-20 15:02:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express =  require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -30,35 +31,39 @@ app.get("/register",function(req,res){
     res.render("register");
 })
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-     if(err){
-         console.log(err);
-     }else{
-         res.render("secrets");
-     }
-    });
+    bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+         if(err){
+             console.log(err);
+         }else{
+             res.render("secrets");
+         }
+        });  
+    });  
 })
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({email:username},function(err,foundUser){//here the password gets decrypt and then login fields get checked
       if(err){
           console.log(err);
       }
       else{
           if(foundUser){
-              if(foundUser.password === password){
-                  res.render("secrets");
-              }
+            bcrypt.compare(password, foundUser.password).then(function(result) {
+                if(result == true){
+                    res.render("secrets");
+                }
+            });
           }
       }
     })
 })
 
-app.listen(4000,function(){
+app.listen(1000,function(){
     console.log("server running on port 3000");
 })
